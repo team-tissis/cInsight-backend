184d5436539a9f846d6b7942d54b5d2c18bc3d16
kato atsushi <ka.baseball1997@gmail.com>
2023-02-15 15:02:57 +0900

    add comment make admin.py valid

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import Comment
+from .models import Favorite
+from .models import Lecture
+from .models import Proposal
+from .models import CustomeUser
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Comment, PostAdmin)
+admin.site.register(Favorite, PostAdmin)
+admin.site.register(Lecture, PostAdmin)
+admin.site.register(Proposal, PostAdmin)
+admin.site.register(CustomeUser, PostAdmin)
--- api/models/favorite.py
+++ api/models/favorite.py
@@ -5,10 +5,10 @@ from api.models.user import CustomeUser
 from api.models.lecture import Lecture
 
 class Favorite(models.Model):
-    user = models.ForeignKey(CustomeUser, on_delete=models.CASCADE, null=False)
-    lecture = models.ForeignKey(Lecture, on_delete=models.CASCADE, null=False)
+    user = models.ForeignKey(CustomeUser, on_delete=models.CASCADE, null=False, related_name="favorites")
+    lecture = models.ForeignKey(Lecture, on_delete=models.CASCADE, null=False, related_name="favorites")
     is_synced = models.BooleanField(help_text='オンチェーンにデータを保存できたらTrue', default=False)
-    vote_weight = models.IntegerField(default=1, null=True)
+    vote_weight = models.IntegerField(default=1)
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
 
