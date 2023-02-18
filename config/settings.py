0e34173cceb633876a15b13c4a1a73ec28157262
kato atsushi <ka.baseball1997@gmail.com>
2023-02-18 19:19:07 +0900

    fix DATABASES

--- config/settings.py
+++ config/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'shion',
-        'PASSWORD': '666666',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
