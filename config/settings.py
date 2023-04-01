78255b8327558886d2a7925c499c553837b2b646
kato atsushi <ka.baseball1997@gmail.com>
2023-04-01 16:49:48 +0900

    reset DATABASES

--- config/settings.py
+++ config/settings.py
@@ -87,9 +87,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'c-insightdb',
-        'USER': 'gonza',
-        'PASSWORD': '666666',
+        'NAME': 'postgres',
+        'USER': 'shion',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
